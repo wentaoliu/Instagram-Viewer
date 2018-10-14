dd5e1d7abe87df044770df573305a7499d5ec8fd
Wentao Liu <liuwnt@gmail.com>
2018-10-14 21:42:53 +1100

    basic ui for activity feed

---
+++ app/src/main/java/com/instagram/instagram_viewer/ActivityFeedFragment.java
@@ -0,0 +1,154 @@
+package com.instagram.instagram_viewer;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TableLayout;
+import android.widget.TextView;
+import android.support.design.widget.TabLayout;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * to handle interaction events.
+ * Use the {@link ActivityFeedFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ActivityFeedFragment extends Fragment {
+
+    //private OnFragmentInteractionListener mListener;
+
+    MyAdapter mAdapter;
+    ViewPager mViewPager;
+    TabLayout mTabLayout;
+
+
+    public ActivityFeedFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment ActivityFeedFragment.
+     */
+    public static ActivityFeedFragment newInstance() {
+        ActivityFeedFragment fragment = new ActivityFeedFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        if (container == null) {
+            return null;
+        }
+        View view = inflater.inflate(R.layout.fragment_activity_feed, container, false);
+
+        // ViewPager and its adapters use support library
+        // fragments, so use getSupportFragmentManager.
+        mTabLayout = (TabLayout) view.findViewById(R.id.tabLayout);
+        mAdapter = new MyAdapter(getFragmentManager(),2 );
+        mViewPager = (ViewPager) view.findViewById(R.id.pager);
+        mViewPager.setAdapter(mAdapter);
+//        mTabLayout.setupWithViewPager(mViewPager);
+        // Inflate the layout for this fragment
+        mTabLayout.setupWithViewPager(mViewPager);
+        return view;
+    }
+
+
+    /*
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    /*
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+    */
+
+
+    public static class MyAdapter extends FragmentPagerAdapter {
+        private int numOfTabs;
+        private String[] tabTitles = new String[]{"Following", "You"};
+
+        public MyAdapter(FragmentManager fm, int numOfTabs) {
+            super(fm);
+            this.numOfTabs = numOfTabs;
+        }
+
+        // overriding getPageTitle()
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return tabTitles[position];
+        }
+
+        @Override
+        public int getCount() {
+            return numOfTabs;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return FollowingActivityFeedFragment.newInstance(position);
+                case 1:
+                    return SelfActivityFeedFragment.newInstance(position);
+                default:
+                    return null;
+            }
+
+        }
+    }
+
+}
